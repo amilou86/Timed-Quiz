01b7960486a442dfeac2ded794a6124f6bf3c36f
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-01-12 17:58:19 +0000

    quiz questions & answers

---
+++ assets/JS/questions.js
@@ -0,0 +1,21 @@
+// quiz questions
+var questions = [
+    {
+        question: "QWhat does HTML stand for?",
+        answers: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        correctAnswer: 3
+    }
+
+    {
+        question: "What does CSS stand for?",
+        answers: ["Cascading Style Sheets", "Character Syllabus Style", "Cascading Simple Sheets", "Character Styling Sheets"],
+        correctAnswer: 1
+    }
+
+      {
+        question: "What does JS stand for?",
+        answers: ["Java Script", "JavaScript", "JivaScrapt", "Jelly Sandwich"],
+        correctAnswer: 2
+    }
+
+];
