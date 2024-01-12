10e3f646eccad49f9824fb2f4f247e8ee6a6787d
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-01-12 16:46:27 +0000

    timer function

--- assets/JS/logic.js
+++ assets/JS/logic.js
@@ -1,10 +1,24 @@
 // create objects
 var startButton = document.getElementById('start-button')
+var questionContainer = document.getElementById('question-container')
 
+// create method to start game 
 startButton.addEventListener('click', startGame)
 console.log('Started')
+startButton.classList.add('hide')
+questionContainer.classList.remove('hide')
+
+var timeRemaining = 60;
+
 // functions needed
 function startGame() {
+    timer = setInterval(() => {
+        timeRemaining--;
+        document.getElementById("timer").textContent = `Time: ${timeRemaining}`;
+        if (timeRemaining === 0) {
+            endGame();
+        }
+    }, 1000);
 
 }
 
